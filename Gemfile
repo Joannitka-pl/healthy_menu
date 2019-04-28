1ee4d5573e55d8d1d7fc2edf3493b1423c234ed1
Joasia <joasia@joasias-macbook-pro.home>
2019-04-28 17:04:12 +0200

    add nick to users

--- Gemfile
+++ Gemfile
@@ -41,11 +41,13 @@ gem 'devise'
 gem 'omniauth-google-oauth2'
 
 # Views
-#gem 'haml'
+# gem 'haml'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: %i[mri mingw x64_mingw]
+  gem 'factory_girl_rails', '~> 4.9'
+  gem 'rspec-rails', '~> 3.8'
 end
 
 group :development do
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,11 @@ class User < ApplicationRecord
   def self.from_omniauth(access_token)
     data = access_token.info
     email = data['email']
-    user = User.where(email: email).first
+    user = User.find_by(email: email)
 
     unless user
       password = Devise.friendly_token[0, 20]
-      user = User.create(email: email, password: password,
+      user = User.create(nick: data['nick'], email: email, password: password,
                          password_confirmation: password)
     end
     user
