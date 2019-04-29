03687eafb0210796d69bc2d96f8b28df5a4b8a8d
Joasia <joasia@joasias-macbook-pro.home>
2019-04-29 14:38:21 +0200

    see all and specific, edit dish

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -1,5 +1,7 @@
 class DishesController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_available_dishes, except: %i[new create]
+  before_action :set_dish, only: %i[show edit update]
 
   def new
     @dish = Dish.new
@@ -16,9 +18,35 @@ class DishesController < ApplicationController
     end
   end
 
+  def edit; end
+
+  def update
+    if @dish.update(check_params)
+      redirect_to dish_path, notice: 'Dish has been changed'
+    else
+      flash.now[:alert] = @dish.errors.full_messages.to_sentence
+      render 'edit'
+    end
+  end
+
+  def show; end
+
+  def index
+    @dishes = Dish.all
+  end
+
   private
 
   def check_params
     params.require(:dish).permit(:name, :details, :public)
+  end
+
+  def set_available_dishes
+    @dishes = Dish.public_or_own_dishes(current_user).all
+  end
+
+  def set_dish
+    @dish = Dish.find(params[:id])
+    render file: Rails.root.join('public', '404.html'), status: :not_found if @dish.blank?
   end
 end
